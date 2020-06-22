7f97386efcfc2ae7744658928c06da672d0fd8f8
Milos Marinkovic <hemicar92@gmail.com>
2020-06-22 23:33:01 +0200

    Basic app layout | in progress

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
-// import { CommonModule } from '@angular/common';
 import { JSMaterialModule } from '../js-material.module';
 
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LayoutComponent } from './layout/layout.component';
 import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
     imports: [ 
-        // CommonModule, 
         JSMaterialModule 
     ],
     declarations: [
+        LayoutComponent,
+        HomeComponent,
+        NavigationComponent,
         LoaderComponent
     ],
     exports: [
+        LayoutComponent,
+        HomeComponent,
+        NavigationComponent,
         LoaderComponent
     ]
 })
---
+++ src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,13 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+    selector: 'app-navigation',
+    templateUrl: './navigation.component.html',
+    styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent {
+
+    @Output() toggleSidenav = new EventEmitter;
+
+    constructor() {}
+}
