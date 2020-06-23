e489ca3a4decf67616050af2ff928536ef6b8af7
Milos Marinkovic <hemicar92@gmail.com>
2020-06-24 01:20:45 +0200

    Component routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
+import { ComponentsRoutingModule } from './components/components-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -13,7 +14,8 @@ import { ComponentsModule } from './components/components.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    // AppRoutingModule,
+    ComponentsRoutingModule,
     BrowserAnimationsModule,
     ComponentsModule
   ],
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,27 +1,38 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JSMaterialModule } from '../js-material.module';
+import { ComponentsRoutingModule } from './components-routing.module';
 
 import { HomeComponent } from './home/home.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { LayoutComponent } from './layout/layout.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductsComponent } from './products/products.component';
 import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
     imports: [
         CommonModule,
-        JSMaterialModule 
+        ComponentsRoutingModule,
+        JSMaterialModule
     ],
     declarations: [
         LayoutComponent,
         HomeComponent,
         NavigationComponent,
+        AboutComponent,
+        ContactComponent,
+        ProductsComponent,
         LoaderComponent
     ],
     exports: [
         LayoutComponent,
         HomeComponent,
         NavigationComponent,
+        AboutComponent,
+        ContactComponent,
+        ProductsComponent,
         LoaderComponent
     ]
 })
