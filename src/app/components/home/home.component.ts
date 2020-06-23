154a79a563bc6dba40c17e5f2c036f7fefe1e178
Milos Marinkovic <hemicar92@gmail.com>
2020-06-23 17:13:12 +0200

    Basis SASS structure added, home page component finished

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
     styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+    homeTitle = 'Welcome to our store';
+    homeDescription = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, tempore nemo provident sunt quisquam minima repudiandae, laudantium veritatis deleniti excepturi at vero! Ipsum magnam, sunt corrupti asperiores inventore porro, atque, maxime aliquid possimus quas provident.';
+
     constructor() {}
 }
