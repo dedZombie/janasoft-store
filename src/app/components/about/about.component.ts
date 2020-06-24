6cd818c72739b9e5efaa88ed93a6c3ea96ef3306
Milos Marinkovic <hemicar92@gmail.com>
2020-06-24 14:27:34 +0200

    Home page refactor, about page added

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -5,5 +5,13 @@ import { Component } from '@angular/core';
     templateUrl: './about.component.html'
 })
 export class AboutComponent {
+
+    aboutTitle = 'About us';
+    aboutDescription = [
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, tempore nemo provident sunt quisquam minima repudiandae, laudantium veritatis deleniti excepturi at vero! Ipsum magnam, sunt corrupti asperiores inventore porro, atque, maxime aliquid possimus quas provident.',
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, tempore nemo provident sunt quisquam minima repudiandae, laudantium veritatis deleniti excepturi at vero! Ipsum magnam, sunt corrupti asperiores inventore porro, atque, maxime aliquid possimus quas provident.',
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, tempore nemo provident sunt quisquam minima repudiandae, laudantium veritatis deleniti excepturi at vero! Ipsum magnam, sunt corrupti asperiores inventore porro, atque, maxime aliquid possimus quas provident.'
+    ];
+
     constructor() {}
 }
