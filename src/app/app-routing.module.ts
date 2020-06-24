842e9a20888034ae2e30076c2e391815bbca47ad
Milos Marinkovic <hemicar92@gmail.com>
2020-06-24 10:26:38 +0200

    Initial component routing finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// import { HomeComponent } from './components/home/home.component';
+// import { AboutComponent } from './components/about/about.component';
+// import { ContactComponent } from './components/contact/contact.component';
+// import { ProductsComponent } from './components/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    // { path: '', redirectTo: '/home', pathMatch: 'full' },
+    // { path: 'home', component: HomeComponent },
+    // { path: 'about', component: AboutComponent },
+    // { path: 'contact', component: ContactComponent },
+    // { path: 'products', component: ProductsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
